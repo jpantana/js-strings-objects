e79d032bf5a5fb6b1eb0619693e1f436bc259e8a
JPantana <joshpantana@me.com>
2019-03-04 21:42:16 -0600

    added stuff

--- main.js
+++ main.js
@@ -49,26 +49,72 @@ printToDom('thirsty', iAmThirsty(67));
 // const quote = 'winter is coming';
 // console.log(quote.indexOf('is')); 
 
+const catName = () => {
+    return 'killer';
+};
+
+const bucketOCats = {
+    cat1: 'fluffy',
+    cat2: 5,
+    cat3: catName,
+    cat4: {
+        water: 'nope',
+        sunlight: 'yep',
+    }
+};
+// dot notation
+console.log(bucketOCats.cat2);
+console.log(bucketOCats.cat4.water);
+// bracket notation
+console.log(bucketOCats['cat2']);
+
+
+let hitchhikers_guide = {
+    characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "Don't Panic",
+    random_facts: {
+        copies_sold: 14000000,
+        formats: ["radio", "TV", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life: 42
+        }
+    }
+};
+
+
+console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life); //42
+console.log(hitchhikers_guide['random_facts']['ultimate_answer']['meaning_of_life']); //42
+
+
+let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010",
+    vacation_days: 20,
+  }
+
+  let eom = "employee_of_the_month";
+  employee[eom] = 'false';
+
+  const answers = [aA, bB, cC, dD]
+
+  var aA = (`Our company's lawyer is ${employee.name}</br>`); 
+  var bB = (`Jeff was hired on ${employee['hire_date']}</br>`);  
+  var cC = (employee.name + ' has ' + employee.vacation_days + ' remaining.</br>');
+  var dD = (employee['employee_of_the_month']);
 
 
 
 
+  console.log(`Our company's lawyer is ${employee.name}`); 
+  console.log(`Jeff was hired on ${employee['hire_date']}`);  
+  console.log(employee.name + ' has ' + employee.vacation_days + ' remaining.');
+  console.log(employee);
 
 
 
-
-// const arrayAge = [55, 21, 11, 44];
-
-// for (var i = 0; i < arrayAge.length; i++) {
-//     if (arrayAge[i] < 21) {
-//         return 'Maybe just drink water';
-//     } else if (arrayAge[i] >= 21 && arrayAge[i] <= 65) {
-//         return 'Have a beer!';
-//     } else {
-//         return 'take a nap'
-//     }
-
-    
-
-// };
-// printToDom('thirsty', arrayAge[i]);
+  printToDom('answers', aA);
+  printToDom('answers', bB);
+  printToDom('answers', cC);
+  printToDom('answers', dD);
